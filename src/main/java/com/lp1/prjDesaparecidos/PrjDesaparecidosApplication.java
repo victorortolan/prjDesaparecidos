222d19639c80fb8776af84d07af5e42d3073595c
alvescode <videoliva@gmail.com>
2024-06-02 10:06:37 -0300

    Template_Inicial

---
+++ src/main/java/com/lp1/prjDesaparecidos/PrjDesaparecidosApplication.java
@@ -0,0 +1,13 @@
+package com.lp1.prjDesaparecidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjDesaparecidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjDesaparecidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lp1/prjDesaparecidos/PrjDesaparecidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp1.prjDesaparecidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjDesaparecidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
