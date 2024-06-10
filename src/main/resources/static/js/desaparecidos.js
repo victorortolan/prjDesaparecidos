d92666981dc55089352566a86ffd766c155efa09
alvescode <videoliva@gmail.com>
2024-06-09 21:24:56 -0300

    adiciona retorno visual ao cadastro de desaparecido

--- src/main/resources/static/js/desaparecidos.js
+++ src/main/resources/static/js/desaparecidos.js
@@ -1,5 +1,5 @@
 const botao_add = document.getElementById("botao_add_desaparecido");
-
+const text = document.getElementById("texto_feedback");
 botao_add.addEventListener("click", async (e) => {
   e.preventDefault();
   // const genero = document.getElementById("genero");
@@ -11,10 +11,16 @@ botao_add.addEventListener("click", async (e) => {
     documento: document.getElementById("documento").value,
     cidade: document.getElementById("cidade").value,
   };
-  console.log(JSON.stringify(formData));
   const response = await fetch("http://localhost:8080/api/v1/adiciona", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(formData),
   });
+
+  const response_text = await response.text();
+  if (response_text == "Adicionado.") {
+    text.innerHTML = `<h3 style="color:green;margin-top:70px;">Desaparecido Adicionado.</h3>`;
+  } else {
+    text.innerHTML = `<h3 style="color:red;margin-top:70px;"}}>Não é possível cadastrar desaparecido.</h3>`;
+  }
 });
